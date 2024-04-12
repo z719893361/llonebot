8d3c4be80750a7359f13ea9379f9ffed2c9ef4a4
gesoy <z719893361@gmail.com>
2024-04-13 03:33:38 +0800

    修改消息顺序

--- onebot/dtypes/message.py
+++ onebot/dtypes/message.py
@@ -1,46 +1,62 @@
+from collections import deque
+
+from onebot.exceptions import BuildMessageError
+
+
 class MessageBuilder:
     def __init__(self):
-        self.message = []
+        self._message = deque()
+        self._exists = set()
+
+    def to_list(self) -> list[dict]:
+        return list(self._message)
 
     def video(self, file: str):
-        self.message = [{
+        if len(self._message) > 0:
+            raise BuildMessageError('视频为独立消息，不能和其他消息一同发送')
+        self._message.append({
             'type': 'video',
             'data': {
                 'file': file
             }
-        }]
-
+        })
         return self
 
     def record(self, file: str):
-        self.message = [{
+        if len(self._message) > 0:
+            raise BuildMessageError('语音是独立消息，不能和其他消息一同发送')
+        self._message.append({
             'type': 'record',
             'data': {
                 'file': file
             }
-        }]
+        })
         return self
 
     def json(self, data: str):
-        self.message = [{
+        if len(self._message) > 0:
+            raise BuildMessageError('JSON是独立消息，不能和其他消息一同发送')
+        self._message.append({
             'type': 'json',
             'data': {
                 'file': data
             }
-        }]
+        })
         return self
 
     def file(self, file: str):
-        self.message = [{
+        if len(self._message) > 0:
+            raise BuildMessageError('文件是独立消息，不能和其他消息一同发送')
+        self._message.append({
             'type': 'file',
             'data': {
                 'file': file
             }
-        }]
+        })
         return self
 
     def text(self, text):
-        self.message.append({
+        self._message.append({
             'type': 'text',
             'data': {
                 'text': text
@@ -49,7 +65,7 @@ class MessageBuilder:
         return self
 
     def image(self, file: str):
-        self.message.append({
+        self._message.append({
             'type': 'image',
             'data': {
                 'file': file
@@ -58,7 +74,7 @@ class MessageBuilder:
         return self
 
     def face(self, face_id: int):
-        self.message.append({
+        self._message.append({
             'type': 'face',
             'data': {
                 'id': str(face_id)
@@ -67,16 +83,21 @@ class MessageBuilder:
         return self
 
     def reply(self, message_id: int):
-        self.message.append({
+        if 'reply' in self._exists:
+            raise BuildMessageError('仅可以设置一条消息回复')
+        index = 0
+        data = {
             'type': 'reply',
             'data': {
                 'id': message_id
             }
-        })
+        }
+        self._message.insert(index, data)
+        self._exists.add('reply')
         return self
 
     def at(self, qq: int):
-        self.message.append({
+        self._message.append({
             'type': 'at',
             'data': {
                 'qq': qq
