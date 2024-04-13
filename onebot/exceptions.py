50dcd177b69c3429ef8c56567e244958f5a09639
gesoy <z719893361@gmail.com>
2024-04-13 16:27:58 +0800

    新增参数异常

--- onebot/exceptions.py
+++ onebot/exceptions.py
@@ -1,3 +1,7 @@
+from inspect import Parameter, getfile
+from typing import Callable
+
+
 class SendMessageError(Exception):
     """
     发送消息错误
@@ -16,3 +20,21 @@ class BuildMessageError(Exception):
 
     def __str__(self):
         return self.msg
+
+
+class RegisterTaskError(Exception):
+    def __init__(self, msg):
+        self.msg = msg
+
+    def __str__(self):
+        return self.msg
+
+
+class ParameterError(Exception):
+    def __init__(self, message: str, fn: Callable, parameter: Parameter):
+        self.msg = message
+        self.fn = fn
+        self.parameter = parameter
+
+    def __str__(self):
+        return f"{self.msg}, 函数: {self.fn.__name__} 参数: {self.parameter.name} 文件: {getfile(self.fn)}"
