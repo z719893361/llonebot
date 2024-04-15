43eaa9156294e03750a69538de637373b57a8451
gesoy <z719893361@gmail.com>
2024-04-15 17:39:24 +0800

    新增好友消息过滤

--- onebot/fliters/impl/message.py
+++ onebot/fliters/impl/message.py
@@ -1,2 +1,9 @@
-# TODO 消息过滤器待实现
+from onebot.fliters import Filter
 
+
+class FriendMessage(Filter):
+    """
+    过滤好友消息
+    """
+    async def support(self, app, message: dict, context: dict) -> bool:
+        return 'group_id' not in message and 'user_id' in message
