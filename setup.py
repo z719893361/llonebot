0a1a1c116bfa229ab9ade22aa1382a56b3f94ff3
gesoy <z719893361@gmail.com>
2024-04-15 23:19:29 +0800

    修改介绍

--- setup.py
+++ setup.py
@@ -1,12 +1,20 @@
 import setuptools
 
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
 
 setuptools.setup(
     name='llonebot',
-    version='1.0.0',
-    author='格索伊',
+    version='1.0.3',
+    author='Gesoy',
     author_email='719893361@qq.com',
-    description='NTQQ llonebot插件 python交互包',
+    description='NTQQ LLOneBot 插件交互工具',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/z719893361/llonebot",
+    project_urls={
+        "Bug Tracker": "https://github.com/z719893361/llonebot/issues",
+    },
     packages=setuptools.find_packages(),
     install_requires=[
         'websockets',
