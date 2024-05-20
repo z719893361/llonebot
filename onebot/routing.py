5c6a739746f2850cc417bcdb73709d620d94e993
gesoy <z719893361@gmail.com>
2024-05-21 04:56:40 +0800

    增加路由顺序和提前结束

--- onebot/routing.py
+++ onebot/routing.py
@@ -1,22 +1,23 @@
 import asyncio
-import time
 from typing import Callable, List, Tuple
 from inspect import iscoroutinefunction, Parameter
 
 import aiocron
 from loguru import logger
 
-from onebot.filter.interfaces import Filter
+from onebot.filter.interfaces import FilterInterface
 from onebot.parameter import Resolver
 from onebot.parameter.composite import PARAMETER_RESOLVER
 
 
 class Route:
-    def __init__(self, func: Callable, filters: List[Filter]):
+    def __init__(self, func: Callable, filters: List[FilterInterface], order: int, continue_: bool):
         self.func = func
-        self.filters = [] if filters is None else filters
+        self.filters = filters
         self.is_async = iscoroutinefunction(func)
         self.resolvers = PARAMETER_RESOLVER.get_function_resolvers(func)
+        self.order = order
+        self.continue_ = continue_
 
     async def matches(self, scope: dict):
         for f in self.filters:
@@ -45,12 +46,12 @@ class Event:
         self.parameters = PARAMETER_RESOLVER.get_parameter_resolver(func)
         self.is_async = iscoroutinefunction(func)
 
-    async def __call__(self, scope: dict, after_close: List[Parameter]):
+    async def __call__(self, scope: dict, after_close_param: List[Parameter]):
         param_values = []
         for param in self.parameters:
             if await PARAMETER_RESOLVER.support(param, scope):
                 param_values.append(await PARAMETER_RESOLVER.resolve(param, scope))
-                after_close.append(param)
+                after_close_param.append(param)
             else:
                 param_values.append(None)
         if self.is_async:
@@ -65,24 +66,29 @@ class Router:
         self.on_startup: List[Event] = []
         self.on_shutdown: List[Event] = []
 
-    def register(self, func: Callable, filters: List[Filter]):
+    def register(self, func: Callable, filters: List[FilterInterface], order: int, continue_: bool):
         route = Route(
             func=func,
-            filters=filters
+            filters=filters,
+            order=order,
+            continue_=continue_
         )
         self.routes.append(route)
+        self.routes.sort(key=lambda r: r.order)
 
     async def __call__(self, scope: dict):
-        param_close = []
+        after_close_param = []
         exc = None
         try:
             for route in self.routes:
                 if await route.matches(scope):
-                    await route.handle(scope, param_close)
+                    await route.handle(scope, after_close_param)
+                    if not route.continue_:
+                        break
         except Exception as e:
             logger.exception(e)
             exc = e
-        for param, resolver in param_close:
+        for param, resolver in after_close_param:
             try:
                 await resolver.close(param, scope, exc)
             except Exception as e:
@@ -104,24 +110,24 @@ class Router:
             self.on_shutdown.append(event)
 
     @staticmethod
-    async def run_handlers(handlers: List[Callable], scope: dict):
-        param_close = []
+    async def run_event_handlers(handlers: List[Callable], scope: dict):
+        after_close_param = []
         for handler in handlers:
             try:
-                await handler(scope, param_close)
+                await handler(scope, after_close_param)
             except Exception as e:
                 logger.exception(e)
-        for param, resolver in param_close:
+        for param, resolver in after_close_param:
             try:
                 await resolver.close(param, {})
             except Exception as e:
                 logger.exception(e)
 
     async def startup(self, scope: dict):
-        await self.run_handlers(self.on_startup, scope)
+        await self.run_event_handlers(self.on_startup, scope)
 
     async def shutdown(self, scope: dict):
-        await self.run_handlers(self.on_shutdown, scope)
+        await self.run_event_handlers(self.on_shutdown, scope)
 
     @staticmethod
     def crontab(func: Callable, spec: str, scope: dict):
@@ -139,9 +145,10 @@ class Router:
                     await resolver.close(param, scope)
                 except Exception as e:
                     logger.exception(e)
+
         # 预缓存参数列表
         parameter_resolver = PARAMETER_RESOLVER.get_function_resolvers(func)
-        # 方法异步状态
+        # 方法是否为异步
         method_async_state = iscoroutinefunction(func)
         # 启动定时任务
         aiocron.crontab(spec, crontab)
